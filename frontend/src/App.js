d487e117688ee92ba6c2f6225b2222f51e1c4364
MOHAMED842022 <EL-BOUTI.MOHAMED@ofppt-edu.ma>
2025-10-20 17:04:22 +0100

    frent modifh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import TaskList from "./components/Tasks/TaskList";
-import TaskForm from "./components/Tasks/TaskForm";
 import TaskFormPage from "./components/Tasks/TaskForm";
 
 function App() {
